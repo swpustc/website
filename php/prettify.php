a71721ac5bff2114317fdee5bce14fd5bd8fe702
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-01-17 10:48:25 +0800

    move php and static documents
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

--- php/prettify.php
+++ php/prettify.php
@@ -9,16 +9,17 @@ $html = htmlspecialchars($text);
     <title><?php echo basename($_SERVER['DOCUMENT_ROOT'] . $_SERVER['SCRIPT_NAME']); ?></title>
     <link href="/static/prettify/prettify.css" type="text/css" rel="stylesheet" />
     <script type="text/javascript" src="/static/prettify/prettify.js"></script>
+    <link href="/static/prettify-body.css" type="text/css" rel="stylesheet" />
   </head>
   <body onload="prettyPrint()">
     <a name="top" id="top"></a>
     <code><a href="javascript:history.go(-1);">back</a>
-      <a href="#end">end</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<script type="text/javascript">
-document.write('Download Raw file: <a href="/__really_download'+document.location.pathname+'"><?php echo basename($_SERVER['DOCUMENT_ROOT'] . $_SERVER['SCRIPT_NAME']); ?></a>');
+      <a href="#end">end</a>&nbsp;&nbsp;&nbsp;&nbsp;<script type="text/javascript">
+document.write('Download: <a href="/download'+document.location.pathname+'" type="application/octet-stream"><?php echo basename($_SERVER['DOCUMENT_ROOT'] . $_SERVER['SCRIPT_NAME']); ?></a>');
     </script></code>
-    <pre class="prettyprint"><code><?php echo $html; ?></code></pre>
+    <pre class="prettyprint linenums"><?php echo $html; ?></pre>
     <code><a href="javascript:history.go(-1);">back</a>
-      <a href="#top">top</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Google code prettify:
+      <a href="#top">top</a>&nbsp;&nbsp;&nbsp;&nbsp;Prettify:
       <a href="https://code.google.com/p/google-code-prettify/">Source</a>
       <a href="http://www.apache.org/licenses/LICENSE-2.0">License</a>
     </code>
