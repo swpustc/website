7d3da3ab46c686bcf97c54127579d7dfb5b854c7
Wanpeng Song <swpustc@mail.ustc.edu.cn>
2015-02-28 10:57:48 +0800

    add php autoindex
    Signed-off-by: Wanpeng Song <swpustc@mail.ustc.edu.cn>

---
+++ php/autoindex.php
@@ -0,0 +1,62 @@
+<?php
+$pwd = $_SERVER['DOCUMENT_ROOT'].$_SERVER['SCRIPT_NAME'];
+$bgn = strlen($_SERVER['DOCUMENT_ROOT']);
+$cur = substr(dirname($pwd).'/'.basename($pwd), $bgn).'/';
+$it  = new FilesystemIterator($pwd);
+?>
+<html lang="zh-CN">
+<head>
+ <meta charset="utf-8">
+ <title>Index of <?php echo $cur ?></title>
+</head>
+<body bgcolor="white">
+ <h1>Index of <?php echo $cur ?></h1><hr>
+ <pre><?php
+$left_part  = 74;
+$right_part = 18;
+$date_size  = 24;
+$dircount  = 0;
+$filecount = 0;
+$dirhtml[$dircount++] = '<a href="'.substr(dirname($pwd), $bgn).'/">../</a>'.str_pad('', $left_part - 3,' ',STR_PAD_LEFT).str_pad('_', $right_part, ' ', STR_PAD_LEFT)."\n";
+foreach ($it as $file){
+  $date = date('Y-m-d H:i:s', $file->getCTime());
+  if ($file->isDir()) {
+    $fileSize = '_';
+    $fileName = $file->getFilename().'/';
+    $fileNameGBK = iconv("UTF-8", "GBK//IGNORE", $fileName);
+    if (strlen($fileNameGBK) > $left_part - $date_size + 4) {
+      $fileNameGBK = mb_strcut($fileNameGBK, 0, $left_part - $date_size, 'GBK').' ...';
+      $fileName = iconv("GBK", "UTF-8//IGNORE", $fileNameGBK);
+    }
+    $pathName = $file->getPathname().'/';
+    $dirhtml[$dircount++] = '<a href="'.substr($pathName, $bgn).'">'.$fileName.'</a>'.str_pad($date, $left_part - strlen($fileNameGBK),' ',STR_PAD_LEFT).str_pad($fileSize, $right_part, ' ', STR_PAD_LEFT)."\n";
+  } elseif ($file->isFile()) {
+    $fileSize = $file->getSize();
+    if ($fileSize < 0)
+      $fileSize += 4294967296;
+    if ($fileSize < 1024);
+    elseif ($fileSize < 1024 * 1024)
+      $fileSize  = round($fileSize / 1024, 2).'K';
+    elseif ($fileSize < 1024 * 1024 * 1024)
+      $fileSize  = round($fileSize / 1024 / 1024, 2).'M';
+    elseif ($fileSize < 1024 * 1024 * 1024 * 1024)
+      $fileSize  = round($fileSize / 1024 / 1024 / 1024, 2).'G';
+    $fileName = $file->getFilename();
+    $fileNameGBK = iconv("UTF-8", "GBK//IGNORE", $fileName);
+    if (strlen($fileNameGBK) > $left_part - $date_size + 4) {
+      $fileNameGBK = mb_strcut($fileNameGBK, 0, $left_part - $date_size, 'GBK').' ...';
+      $fileName = iconv("GBK", "UTF-8//IGNORE", $fileNameGBK);
+    }
+    $pathName = $file->getPathname();
+    $filehtml[$filecount++] = '<a href="'.substr($pathName, $bgn).'">'.$fileName.'</a>'.str_pad($date, $left_part - strlen($fileNameGBK),' ',STR_PAD_LEFT).str_pad($fileSize, $right_part, ' ', STR_PAD_LEFT)."\n";
+  }
+}
+for ($i = 0; $i < $dircount; $i++) {
+  echo $dirhtml[$i];
+}
+for ($i = 0; $i < $filecount; $i++) {
+  echo $filehtml[$i];
+}
+ ?></pre><hr>
+</body>
+</html>
